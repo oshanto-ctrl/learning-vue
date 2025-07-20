3ae746961c3a68fb7b204d4dcd629bdf0bb2263c
Rejoan Siddiky <siddikyrejoan@gmail.com>
2025-07-21 00:52:40 +0600

    Started learning vue3 from vue mastery

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+
+    <div>
+      <HelloWorld msg="SOCKS" />
+    </div>
+  </header>
+
+</template>
+
+<style scoped>
+
+</style>
