526c675f9e798b428f3a1416f538bc924e20732d
Rejoan Siddiky <siddikyrejoan@gmail.com>
2025-07-21 22:43:30 +0600

    Learned conditional rendering v-if, v-else-if, v-else, v-show, Logics behind the directives

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,18 +1,37 @@
 <script setup>
   // Logic, state
   import { ref } from 'vue'
+  import socksImage from './assets/images/socks_green.jpeg'
 
   const product = ref("Socks")
-  const p_description = ref("Made With High Quality Cotton For Everyday Use.")
+  const image = ref(socksImage)
+  // const inStock = ref(false)
+  const inventory = ref(12)
+  const onSale = ref(true)
+
 </script>
 
 <template>
   <!-- Skeleton -->
   <div class="product-display">
     <div class="product-container">
+      <div class="product-image">
+        <img :src="image" alt="High Quality Socks">
+      </div>
+      
       <div class="product-info">
         <h1>{{ product }}</h1>
-        <p>{{ p_description }}</p>
+
+        <p v-show="onSale">On Winter Sale!!!</p>
+        <!--
+        <p v-if="inStock">In Stock</p>
+        <p v-else>Out of Stock</p>
+        -->
+        <!-- <p v-show="inStock">In Stock</p> -->
+        <p v-if="inventory > 20">Available in stock.</p>
+        <p v-else-if="inventory <= 20 && inventory > 0">Ending soon! collect yours.</p>
+        <p v-else>Sorry, Out of Stock!</p>
+      
       </div>
     </div>
   </div>
